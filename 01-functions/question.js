dcf1915734f58f779587b685f0a7fd7e9808f1f3
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-05-06 11:33:22 -0600

    challenge 1 complete

--- 01-functions/question.js
+++ 01-functions/question.js
@@ -1,12 +1,17 @@
 // challenge
+//variable 1
 const message = 'your phone number is contained somewhere in the digits of Pi';
+
+// in JS, functions are first class citizens, meaning they can be assigned to variables, passed into other functions and have return types
+
+//challenge: invoke the saysomething and react function in a way that console logs the message variable and invokes one of the reaction methods
 const surprisedReaction = () => console.log(':o');
 const smoothReaction = () => console.log('B)');
-const saySomethingAndReact = (msg = 'you forgot to submit a message') => {
-    console.log(msg);
-    return (reaction) => {
-        reaction();
-    };
+const saySomethingAndReact = () => {
+    console.log(message);
+    
+        surprisedReaction();
+
 };
 
 // add the inputs here
